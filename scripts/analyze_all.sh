65b6e410c0f40c9314077343d392dee8990429f3
Chris Syers <csyers@nd.edu>
2017-04-28 20:15:31 -0400

    results files

--- scripts/analyze_all.sh
+++ scripts/analyze_all.sh
@@ -3,11 +3,11 @@
 for f in ../data/*; do
     O="$(basename $f)"
     Z=${O%.json}
-    eval "python2 analyzer.py $f ../results/${Z}.csv"
+    echo "python2 analyzer.py $f ../results/${Z}.csv"
 done
 
 for f in ../data/*; do
     O="$(basename $f)"
     Z=${O%.json}
-    eval "./wordcloud.sh ../results/${Z}.csv ../images/${Z}.png"
+    echo "./wordcloud.sh ../results/${Z}.csv ../images/${Z}.png"
 done
