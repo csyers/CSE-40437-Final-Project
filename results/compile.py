04ee83eb14017ab57ff7c5443e269ea39aec5a9d
Chris Syers <csyers@nd.edu>
2017-04-30 15:42:10 -0400

    update compile

--- results/compile.py
+++ results/compile.py
@@ -5,17 +5,22 @@ import json
 import sys
 import json
 
-option = sys.argv[2]
-
 return_json = {}
 
 with open(sys.argv[1]) as f:
     return_json["dates"] = [row["date"] for row in DictReader(f)]
     f.seek(0)
-    return_json["ratings"] = [float(row[option]) for row in DictReader(f)]
+    return_json["ratings"] = [float(row["rating"]) for row in DictReader(f)]
+    f.seek(0)
+    return_json["sentiment"] = [float(row["sentiment"]) for row in DictReader(f)]
+
 for rating in return_json["ratings"]:
     print rating,
 print
 for date in return_json["dates"]:
     print date,
+print
+for sentiment in return_json["sentiment"]:
+    print sentiment,
+
 print
