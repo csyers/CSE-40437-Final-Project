12ef3c24c8d01e39794e48578dd63554fb29e2fd
Chris Syers <csyers@nd.edu>
2017-04-29 22:12:25 -0400

    full stack capabilities

---
+++ scripts/full_stack_old.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+keyword=$1
+start_date=$2 
+end_date=$3
+outfile=${keyword// /_}
+tweet_outfile="${outfile}.json"
+csv_outfile="${outfile}.csv"
+wordcloud_outfile="${outfile}.png"
+
+
+
+eval "./get_old_tweets.py \"${keyword}\" $start_date $end_date ../data/$tweet_outfile"
+eval "python2 ./analyzer.py ../data/$tweet_outfile ../results/$csv_outfile"
+eval "./wordcloud.sh ../results/$csv_outfile ../images/$wordcloud_outfile"
