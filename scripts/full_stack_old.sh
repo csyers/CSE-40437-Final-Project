152befd4ea34734a28a1036f7f0b1db476bf05c8
Chris Syers <csyers@nd.edu>
2017-05-01 08:43:48 -0400

    directory fix

--- scripts/full_stack_old.sh
+++ scripts/full_stack_old.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 keyword=$1
 start_date=$2 
@@ -9,6 +10,6 @@ tweet_outfile="${outfile}.json"
 csv_outfile="${outfile}.csv"
 wordcloud_outfile="${outfile}.png"
 
-eval "python3 ./get_old_tweets.py \"${keyword}\" $start_date $end_date ../data/$tweet_outfile"
-eval "python2 ./analyzer.py ../data/$tweet_outfile ../results/$csv_outfile"
-eval "./wordcloud.sh ../results/$csv_outfile ../images/$wordcloud_outfile"
+eval "python3 ${DIR}/get_old_tweets.py \"${keyword}\" $start_date $end_date ${DIR}/../data/$tweet_outfile"
+eval "python2 ${DIR}./analyzer.py ${DIR}../data/$tweet_outfile ${DIR}/../results/$csv_outfile"
+eval "${DIR}./wordcloud.sh ${DIR}../results/$csv_outfile ${DIR}../images/$wordcloud_outfile"
