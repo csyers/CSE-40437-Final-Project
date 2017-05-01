d29435f198df707948cea7e190778a8e7db9e915
Chris Scaramella <CScarame@nd.edu>
2017-05-01 09:27:15 -0400

    bugfixing

--- scripts/full_stack_old.sh
+++ scripts/full_stack_old.sh
@@ -11,5 +11,5 @@ csv_outfile="${outfile}.csv"
 wordcloud_outfile="${outfile}.png"
 
 eval "python3 ${DIR}/get_old_tweets.py \"${keyword}\" $start_date $end_date ${DIR}/../data/$tweet_outfile"
-eval "python2 ${DIR}./analyzer.py ${DIR}../data/$tweet_outfile ${DIR}/../results/$csv_outfile"
-eval "${DIR}./wordcloud.sh ${DIR}../results/$csv_outfile ${DIR}../images/$wordcloud_outfile"
+eval "python2 ${DIR}/analyzer.py ${DIR}/../data/$tweet_outfile ${DIR}/../results/$csv_outfile"
+eval "${DIR}/wordcloud.sh ${DIR}/../results/$csv_outfile ${DIR}/../images/$wordcloud_outfile"
--- website/scripts/ndata.js
+++ website/scripts/ndata.js
@@ -24,3 +24,41 @@ function createNewSearch() {
 		    console.log(err);
 		});
 }
+
+
+ 
+function changeGraph() {
+		var product = $("#newterm").val();
+		console.log(product)
+		$.get({
+				url: "../php/data.php",
+				data: {
+						product: product
+				}
+		}).done(function(output, status){
+				var obj = JSON.parse(output);
+				dates = obj[1].split(" ");
+				results = obj[0].split(" ");
+				results2 = obj[2].split(" ");
+				data = []
+				data2 = []
+				for(i=0; i< dates.length; i++){
+						data.push({date: dates[i], result: Number(results[i])});
+						data2.push({date: dates[i], result: Number(results2[i])});
+				}
+				graph.setData(data);
+				graph2.setData(data2);
+				                var title = product.split(".")[0].split("_")
+                for (i = 0; i < title.length; i++) {
+										title[i] = title[i].charAt(0).toUpperCase() + title[i].substring(1);
+                }
+				var fixed_title = title.join(" ");
+				$("#title").text(fixed_title);
+				var image = "../../images/" + product.split(".")[0] + ".png"
+				$("#word_cloud").attr("src",image); 
+
+				
+		}).fail(function(err){
+				console.log(err);
+		});
+}
