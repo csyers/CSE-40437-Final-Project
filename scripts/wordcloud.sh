780b6cc30fdd0d3f1369cc903444b1bed55ec278
Chris Syers <csyers@nd.edu>
2017-05-01 08:56:40 -0400

    wordlcoud.sh directory fix

--- scripts/wordcloud.sh
+++ scripts/wordcloud.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # get the file temp.txt to make the cloud out if
-./create_word_cloud_file.py $1
+${DIR}/create_word_cloud_file.py $1
 
 # execute the worldcloud_cli tool to create the file supplied in second argument
-eval 'python2 ./wordcloud_cli.py --imagefile $2 --text temp.txt --background "rgb(255,255,255)" --no_collocations'
+eval 'python2 ${DIR}/./wordcloud_cli.py --imagefile $2 --text temp.txt --background "rgb(255,255,255)" --no_collocations'
 
 # remove the temporary file
-rm temp.txt
+rm ${DIR}/temp.txt
